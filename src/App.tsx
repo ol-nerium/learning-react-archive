9940a0ee8e14a9f84a5e0d836d0d399e8e858a2f
al-burlachenko <alexandrburl@gmail.com>
2025-09-11 17:49:27 +0300

    adds and comments old way to write classes

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,6 @@ import Clicker from '@/components/clicker';
 import { Component } from 'react';
 
 class App extends Component {
-  state: { value: number } = {
-    value: 0,
-  };
-
-  // constructor() {
-  //   super();
-  // }
-
-  increaseValue: (value: number) => void = prevValue => {
-    this.setState({
-      value: prevValue + 1,
-    });
-  };
-
-  decreaseValue: (value: number) => void = prevValue => {
-    this.setState({
-      value: prevValue - 1,
-    });
-  };
-
   render() {
     return (
       <>
--- src/components/clicker.tsx
+++ src/components/clicker.tsx
@@ -38,3 +38,53 @@ export default class Clicker extends Component {
     );
   }
 }
+
+// // more old way to right classes, rewrited and commented for learning reasons
+
+// export default class Clicker extends Component<
+//   { step: number; initialValue: number },
+//   { value: number }
+// > {
+//   // <MyProps, MyState>
+//   static defaultProps: { step: number; initialValue: number } = {
+//     step: 1,
+//     initialValue: 0,
+//   };
+
+//   constructor(props: { step: number; initialValue: number }) {
+//     super(props);
+//     this.state = {
+//       value: this.props.initialValue,
+//     };
+//     this.increaseValue = this.increaseValue.bind(this);
+//     this.decreaseValue = this.decreaseValue.bind(this);
+//   }
+
+//   // increaseValue(evt: React.MouseEvent<HTMLButtonElement>): void {
+//   increaseValue(): void {
+//     this.setState({
+//       value: this.state.value + this.props.step,
+//     });
+//   }
+
+//   // decreaseValue(evt: React.MouseEvent<HTMLButtonElement>): void {
+//   decreaseValue(): void {
+//     this.setState({
+//       value: this.state.value - this.props.step,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <h1>Click-Clack in classes, motherfucker!</h1>
+//         <p>
+//           Value:
+//           <span> {this.state.value}</span>
+//         </p>
+//         <button onClick={this.decreaseValue}>Click -</button>
+//         <button onClick={this.increaseValue}>Click +</button>
+//       </>
+//     );
+//   }
+// }
