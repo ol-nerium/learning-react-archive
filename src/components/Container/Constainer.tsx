95fb2402f3c31df632be9f27afad4bba55f087fb
al-burlachenko <alexandrburl@gmail.com>
2025-09-24 14:15:16 +0300

    adds files from react-cafe project to this new branch

--- src/components/Container/Constainer.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from 'react';
-import css from './Container.module.css';
-
-export default function Container({ children }: { children: ReactNode }) {
-  return <div className={css.container}>{children}</div>;
-}
--- src/utils/types.tsx
+++
@@ -1,5 +0,0 @@
-export interface stateType {
-  name: string;
-  number: string;
-  key: string;
-}
